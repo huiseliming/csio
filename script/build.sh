d708fb2badaa1be97b39ac2e4108070ad72782c7
daimingze <anyingshashou@qq.com>
2021-04-03 14:57:43 +0800

    osx compile

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
-cmake -S . -B out/build
+#!/bin/bash
+cd `dirname $0`
+# build in unix like system?
+# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
+# build macOS-Universal
+# cmake -DCMAKE_OSX_ARCHITECTURES=arm64;x86_64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
+# build macOS-arm64
+cmake -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S .. -B ../build
+# build macOS-x86_64
+cd ../build
+make 
